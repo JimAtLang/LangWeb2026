8f9ee5e136c6717f0374f91de13fd290bef455f9
Conrad Thompson <conrad.thompson@thelangschool.net>
2025-05-16 10:28:13 -0400

    buttons

--- script.js
+++ script.js
@@ -6,25 +6,37 @@ function dropperDown() {
 
 function gradePicker1() {
     document.getElementById("Highline").classList.toggle("flex");
+    document.getElementById("Morningside").classList.toggle("noShow");
+    document.getElementById("Bryant_Sakura").classList.toggle("noShow");
 }
 
 function gradePicker2() {
-    document.getElementById("Highline").classList.toggle("flex");
+    document.getElementById("Highline").classList.toggle("noShow");
+    document.getElementById("Morningside").classList.toggle("flex");
+    document.getElementById("Bryant_Sakura").classList.toggle("noShow");
 }
 
 function gradePicker3() {
-    document.getElementById("Highline").classList.toggle("flex");
+    document.getElementById("Highline").classList.toggle("noShow");
+    document.getElementById("Morningside").classList.toggle("noShow");
+    document.getElementById("Bryant_Sakura").classList.toggle("flex");
 }
 
 function gradePicker4() {
     document.getElementById("Highline").classList.toggle("flex");
+    document.getElementById("Morningside").classList.toggle("flex");
+    document.getElementById("Inwood_Soundview").classList.toggle("flex");
 }
 
 function gradePicker5() {
     document.getElementById("Highline").classList.toggle("flex");
+    document.getElementById("Morningside").classList.toggle("flex");
+    document.getElementById("Upper_School").classList.toggle("flex");
 }
 
 function gradePicker6() {
     document.getElementById("Highline").classList.toggle("flex");
+    document.getElementById("Morningside").classList.toggle("flex");
+    document.getElementById("Staff").classList.toggle("flex");
 }
 
