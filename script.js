7dd056fd9f3bc0072ba899d249eb833ca7f47a91
Conrad Thompson <conrad.thompson@thelangschool.net>
2025-05-16 10:17:13 -0400

    pic buttons

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
 function dropperDown() {
     document.getElementById("navDrop").classList.toggle("show");
 }
+
+// someone make the gradePickers one function please
+
+function gradePicker1() {
+    document.getElementById("Highline").classList.toggle("flex");
+}
+
+function gradePicker2() {
+    document.getElementById("Highline").classList.toggle("flex");
+}
+
+function gradePicker3() {
+    document.getElementById("Highline").classList.toggle("flex");
+}
+
+function gradePicker4() {
+    document.getElementById("Highline").classList.toggle("flex");
+}
+
+function gradePicker5() {
+    document.getElementById("Highline").classList.toggle("flex");
+}
+
+function gradePicker6() {
+    document.getElementById("Highline").classList.toggle("flex");
+}
+
