39dff87598b18190fe2af8f0eb6e263f72953312
Conrad Thompson <conrad.thompson@thelangschool.net>
2025-05-20 15:05:41 -0400

    fixed background, set up more images

--- script.js
+++ script.js
@@ -4,45 +4,66 @@ function dropperDown() {
 
 // someone make the gradePickers one function please
 
+function gradePicker0() {
+    document.getElementById("Highline").classList.add("defaultClass");
+    document.getElementById("Morningside").classList.add("flex");
+    document.getElementById("Bryant_Sakura").classList.add("flex");
+    document.getElementById("Inwood_Soundview").classList.add("flex");
+    document.getElementById("Upper_School").classList.add("flex");
+    document.getElementById("Staff").classList.add("flex");
+}
+
 function gradePicker1() {
-    document.getElementById("Highline").classList.toggle("flex");
-    document.getElementById("Morningside").classList.remove("flex");
-    document.getElementById("Bryant_Sakura").classList.remove("flex");
-}
-
-function gradePicker2() {
-    document.getElementById("Highline").classList.remove("flex");
-    document.getElementById("Morningside").classList.toggle("flex");
-    document.getElementById("Bryant_Sakura").classList.remove("flex");
-}
-
-function gradePicker3() {
-    document.getElementById("Highline").classList.remove("flex");
-    document.getElementById("Morningside").classList.remove("flex");
-    document.getElementById("Bryant_Sakura").classList.toggle("flex");
-}
-
-function gradePicker4() {
-    document.getElementById("Highline").classList.remove("flex");
-    document.getElementById("Morningside").classList.remove("flex");
-    document.getElementById("Bryant_Sakura").classList.remove("flex");
-    document.getElementById("Inwood_Soundview").classList.toggle("flex");
-}
-
-function gradePicker5() {
-    document.getElementById("Highline").classList.remove("flex");
-    document.getElementById("Morningside").classList.remove("flex");
-    document.getElementById("Bryant_Sakura").classList.remove("flex");
-    document.getElementById("Inwood_Soundview").classList.remove("flex");
-    document.getElementById("Upper_School").classList.toggle("flex");
-}
-
-function gradePicker6() {
-    document.getElementById("Highline").classList.remove("flex");
+    document.getElementById("Highline").classList.add("defaultClass");
     document.getElementById("Morningside").classList.remove("flex");
     document.getElementById("Bryant_Sakura").classList.remove("flex");
     document.getElementById("Inwood_Soundview").classList.remove("flex");
     document.getElementById("Upper_School").classList.remove("flex");
-    document.getElementById("Staff").classList.toggle("flex");
+    document.getElementById("Staff").classList.remove("flex");
+}
+
+function gradePicker2() {
+    document.getElementById("Highline").classList.remove("defaultClass");
+    document.getElementById("Morningside").classList.add("flex");
+    document.getElementById("Bryant_Sakura").classList.remove("flex");
+    document.getElementById("Inwood_Soundview").classList.remove("flex");
+    document.getElementById("Upper_School").classList.remove("flex");
+    document.getElementById("Staff").classList.remove("flex");
+}
+
+function gradePicker3() {
+    document.getElementById("Highline").classList.remove("defaultClass");
+    document.getElementById("Morningside").classList.remove("flex");
+    document.getElementById("Bryant_Sakura").classList.add("flex");
+    document.getElementById("Inwood_Soundview").classList.remove("flex");
+    document.getElementById("Upper_School").classList.remove("flex");
+    document.getElementById("Staff").classList.remove("flex");
+}
+
+function gradePicker4() {
+    document.getElementById("Highline").classList.remove("defaultClass");
+    document.getElementById("Morningside").classList.remove("flex");
+    document.getElementById("Bryant_Sakura").classList.remove("flex");
+    document.getElementById("Inwood_Soundview").classList.add("flex");
+    document.getElementById("Upper_School").classList.remove("flex");
+    document.getElementById("Staff").classList.remove("flex");
+}
+
+function gradePicker5() {
+    document.getElementById("Highline").classList.remove("defaultClass");
+    document.getElementById("Morningside").classList.remove("flex");
+    document.getElementById("Bryant_Sakura").classList.remove("flex");
+    document.getElementById("Inwood_Soundview").classList.remove("flex");
+    document.getElementById("Upper_School").classList.add("flex");
+    document.getElementById("Staff").classList.remove("flex");
+}
+
+function gradePicker6() {
+    document.getElementById("Highline").classList.remove("defaultClass");
+    document.getElementById("Morningside").classList.remove("flex");
+    document.getElementById("Bryant_Sakura").classList.remove("flex");
+    document.getElementById("Inwood_Soundview").classList.remove("flex");
+    document.getElementById("Upper_School").classList.remove("flex");
+    document.getElementById("Staff").classList.add("flex");
 }
 
